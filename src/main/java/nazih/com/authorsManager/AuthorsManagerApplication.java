1c8c6890306527f717a0c080ac6dbcbee57eeb2e
Anasse Nazih <nazih.anasse@gmail.com>
2018-08-02 00:43:44 +0200

    First commit - spring boot app template

---
+++ src/main/java/nazih/com/authorsManager/AuthorsManagerApplication.java
@@ -0,0 +1,12 @@
+package nazih.com.authorsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorsManagerApplication.class, args);
+	}
+}
